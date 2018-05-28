3ebeabaea06b805f305c176a4df31322e02f1142
kk2 <kk2@WIN10-KK2>
2018-05-28 12:08:03 +0200

    Hello PPEDV

--- MiddlewareDemo/Startup.cs
+++ MiddlewareDemo/Startup.cs
@@ -27,7 +27,7 @@ namespace MiddlewareDemo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello PPEDV!");
             });
         }
     }
